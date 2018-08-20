e05530e24d5296a5f9b056b8495d6c87d22502d6
rkpatra201 <patrarabindra0@gmail.com>
2018-08-21 04:04:29 +0530

    1.8

--- web/src/main/java/com/spring/nginxbootmysql/NginxBootMysqlApplication.java
+++ web/src/main/java/com/spring/nginxbootmysql/NginxBootMysqlApplication.java
@@ -1,28 +1,26 @@
 package com.spring.nginxbootmysql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 @RestController
 public class NginxBootMysqlApplication {
 
-	@Autowired
-	JdbcTemplate template;
+	//@Autowired
+	JdbcTemplate template=new JdbcTemplate(getDataSource());
 	
 	@Value("${spring.datasource.url}")
 	private String url;
@@ -33,25 +31,20 @@ public class NginxBootMysqlApplication {
 	@Value("${spring.datasource.driver-class-name}")
 	private String driverClass;
 	
+	public DataSource getDataSource()
+	{
+		HikariDataSource ds=new HikariDataSource();
+		ds.setUsername(userName);
+		ds.setPassword(password);
+		ds.setDriverClassName(driverClass);
+		ds.setJdbcUrl(url);
+		
+		return ds;
+	}
 	@PostConstruct
 	public void init() throws SQLException
 	{
-		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(userName);
-        dataSourceBuilder.password(password);
-        dataSourceBuilder.driverClassName(driverClass);
-        template.setDataSource(dataSourceBuilder.build()); 
-        
-        
-        
-        System.out.println("\n\n\n\n\n RETRV1 \n\n\n\n");
-        Connection con=DriverManager.getConnection(url, userName, password);
-        System.out.println(con);
-        System.out.println("\n\n\n\n\n RETRV2 \n\n\n\n");
-        
-        DriverManagerDataSource ds=new DriverManagerDataSource(url, userName, password);
-        template.setDataSource(ds);
+		System.out.println("RETRV");
         getUsers().forEach( e->{
         	System.out.println(e.getEmail());
         });;
