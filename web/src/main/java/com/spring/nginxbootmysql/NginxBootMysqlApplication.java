9a1b118b07bd14bc22faf879bd66a13e4b8fc963
rkpatra201 <patrarabindra0@gmail.com>
2018-08-22 12:11:51 +0530

    Add files via upload

--- web/src/main/java/com/spring/nginxbootmysql/NginxBootMysqlApplication.java
+++ web/src/main/java/com/spring/nginxbootmysql/NginxBootMysqlApplication.java
@@ -1,16 +1,21 @@
 package com.spring.nginxbootmysql;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zaxxer.hikari.HikariDataSource;
@@ -19,8 +24,12 @@ import com.zaxxer.hikari.HikariDataSource;
 @RestController
 public class NginxBootMysqlApplication {
 
-	//@Autowired
-	JdbcTemplate template=new JdbcTemplate(getDataSource());
+	@Autowired
+	JdbcTemplate template;
+	
+	
+	@Autowired
+	HikariDataSource ds;
 	
 	@Value("${spring.datasource.url}")
 	private String url;
@@ -31,27 +40,25 @@ public class NginxBootMysqlApplication {
 	@Value("${spring.datasource.driver-class-name}")
 	private String driverClass;
 	
-	public DataSource getDataSource()
-	{
-		HikariDataSource ds=new HikariDataSource();
-		ds.setUsername(userName);
-		ds.setPassword(password);
-		ds.setDriverClassName(driverClass);
-		ds.setJdbcUrl(url);
-		
-		return ds;
-	}
+	
+	
 	@PostConstruct
 	public void init() throws SQLException
 	{
-		System.out.println("RETRV");
-        getUsers().forEach( e->{
-        	System.out.println(e.getEmail());
-        });;
+		System.out.println(url);
+		System.out.println(userName);
+		System.out.println(password);
+		System.out.println(driverClass);
+		System.out.println("INIT");
+       
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(NginxBootMysqlApplication.class, args);
+		ApplicationContext context=SpringApplication.run(NginxBootMysqlApplication.class, args);
+	
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(e->{
+			System.out.println(e);
+		});
 	}
 
 	@GetMapping("/hello")
@@ -59,8 +66,24 @@ public class NginxBootMysqlApplication {
 		return "Hello from Spring boot";
 	}
 
-	@GetMapping("/users")
-	public List<User> getUsers() {
+	@GetMapping("/users/{host}")
+	public List<User> getUsers(@PathVariable("host") String host) {
+		JdbcTemplate template=new JdbcTemplate(getDataSource(host));
+		//template.setDataSource(getDataSource(host));
 		return template.query("SELECT * FROM TBL_USER", new UserRowMapper());
+	}
+
+	private DataSource getDataSource(String host) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("host: "+host);
+		String jdbcUrl=url.replace("misql", host);
+		System.out.println(jdbcUrl);
+		
+		DriverManagerDataSource ds=new DriverManagerDataSource(jdbcUrl,userName,password);
+		
+		System.out.println(ds.getClass());
+		
+		return ds;
 	}
 }
