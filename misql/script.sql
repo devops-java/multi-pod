11af75febe2f177e6e6bc05c82838655ce795e29
rkpatra201 <patrarabindra0@gmail.com>
2018-08-22 14:24:02 +0530

    create user

--- misql/script.sql
+++ misql/script.sql
@@ -1,4 +1,19 @@
 
+CREATE USER 'monty'@'localhost' IDENTIFIED BY 'some_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'monty'@'localhost' WITH GRANT OPTION;
+CREATE USER 'monty'@'%' IDENTIFIED BY 'some_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'monty'@'%' WITH GRANT OPTION;
+
+CREATE USER 'root'@'%' IDENTIFIED BY 'password1';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+CREATE USER 'admin'@'localhost';
+GRANT RELOAD,PROCESS ON *.* TO 'admin'@'localhost';
+CREATE USER 'dummy'@'localhost';
+FLUSH PRIVILEGES;
+
+SELECT HOST,USER FROM MYSQL.USER;
+
 CREATE DATABASE IF NOT EXISTS MYDB;
 
 USE MYDB;
